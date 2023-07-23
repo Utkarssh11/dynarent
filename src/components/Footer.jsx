ff69673d1496417297b79fb75791985a89976152
Utkarsh Patidar <114795899+Utkarssh11@users.noreply.github.com>
2023-07-23 14:42:48 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-black text-primary text-lg text-center p-2">
+      &#169; 2023 #DYNARENT @utkarsh 
+    </footer>
+  );
+};
+
+export default Footer;
